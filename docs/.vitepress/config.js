0363cbf547d1a44b9738ce6d59645ce01bf01428
ajiho <lujiahao@88.com>
2026-01-05 22:48:46 +0800

    chore: 文档框架初步搭建
    Signed-off-by: ajiho <lujiahao@88.com>

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,51 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "release-pls",
+  base: "/release-pls/",
+  description: "一个简单的发布工具",
+  head: [
+    ["link", { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" }],
+    [
+      "meta",
+      {
+        name: "google",
+        content: `notranslate`,
+      },
+    ],
+  ],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Guide", link: "/guide/getting-started", activeMatch: "/guide/" },
+      {
+        text: "Reference",
+        link: "/reference/cli",
+        activeMatch: "/reference/",
+      },
+    ],
+
+    logo: { src: "/logo-mini.svg", width: 24, height: 24 },
+    sidebar: {
+      "/guide/": [
+        {
+          text: "Guide",
+          base: "/guide/",
+          items: [{ text: "getting-started", link: "getting-started" }],
+        },
+      ],
+
+      "/reference/": [
+        {
+          text: "Reference",
+          base: "/reference/",
+          items: [{ text: "CLI", link: "cli" }],
+        },
+      ],
+    },
+    socialLinks: [
+      { icon: "github", link: "https://github.com/release-pls/release-pls" },
+    ],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  image:
+    src: /logo-large.svg
+    alt: Release-Pls
+  name: Release-Pls
+  text: 轻量级通用发布 CLI
+  tagline: 交互式发布，一气呵成
+  actions:
+    - theme: brand
+      text: Quickstart
+      link: /guide/getting-started
+    - theme: alt
+      text: Github
+      link: https://github.com/release-pls/release-pls
+
+features:
+  - title: 语义化版本
+    details: 自动计算您想发布的语义化版本
+  - title: 变更日志
+    details: 内部集成变更日志功能
+  - title: hooks
+    details: 支持灵活的hooks
+---
